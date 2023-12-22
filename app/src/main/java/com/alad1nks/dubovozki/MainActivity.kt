5c1efc56f61ed90d73bf6eb48e272e0dfa48ff8d
alad1nks <vasya.sidr777@gmail.com>
2023-12-22 18:34:07 +0300

    feat navigation: add navigation graph

--- app/src/main/java/com/alad1nks/dubovozki/MainActivity.kt
+++ app/src/main/java/com/alad1nks/dubovozki/MainActivity.kt
@@ -3,38 +3,15 @@ package com.alad1nks.dubovozki
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import com.alad1nks.components.spinner.MenuItem
-import com.alad1nks.components.spinner.Spinner
 import com.alad1nks.dubovozki.ui.theme.DubovozkiAndroidTheme
+import com.alad1nks.navigation.NavigationGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DubovozkiAndroidTheme {
-                var expanded by remember { mutableStateOf(false) }
-                val items = listOf(
-                    MenuItem("SAS", "sas"),
-                    MenuItem("safsaf", "agags")
-                )
-                var selectedItem by remember { mutableStateOf(items.first()) }
-                Spinner(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false },
-                    onClick = {
-                        expanded = !expanded
-                    },
-                    items = items,
-                    selectedItem = selectedItem,
-                    onSelect = {
-                        selectedItem = it
-                        expanded = false
-                    }
-                )
+                NavigationGraph()
             }
         }
     }
