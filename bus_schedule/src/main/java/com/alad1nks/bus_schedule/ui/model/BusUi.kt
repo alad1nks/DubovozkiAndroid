3556de4d9c7807377c3cc4ee625090b296ddc99f
alad1nks <vasya.sidr777@gmail.com>
2023-12-24 02:29:46 +0300

    feat bus-schedule: add bus and station ui model

--- bus_schedule/src/main/java/com/alad1nks/bus_schedule/ui/model/BusUi.kt
+++
@@ -1,8 +0,0 @@
-package com.alad1nks.bus_schedule.ui.model
-
-import androidx.compose.runtime.Composable
-
-interface BusUi {
-    @Composable
-    fun Content()
-}
---
+++ core/ui/src/main/java/com/alad1nks/core/ui/Bus.kt
@@ -0,0 +1,91 @@
+package com.alad1nks.core.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.alad1nks.core.model.Bus
+
+@Composable
+fun Bus.Content() {
+    when(this) {
+        is Bus.Upcoming -> {
+            Column {
+                ListItem(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    headlineContent = {
+                        if (soon) {
+                            Text(
+                                text = timeLeft,
+                                modifier = Modifier
+                                    .background(
+                                        color = Color.Red,
+                                        shape = MaterialTheme.shapes.small
+                                    )
+                                    .padding(vertical = 8.dp, horizontal = 12.dp),
+                                color = Color.White,
+                                fontSize = 12.sp
+                            )
+                        } else {
+                            Text(
+                                text = timeLeft,
+                                fontSize = 12.sp
+                            )
+                        }
+                    },
+                    leadingContent = station.timeContent(time),
+                    trailingContent = station.nameContent()
+                )
+                Divider()
+            }
+        }
+
+        is Bus.Departed -> {
+            Column {
+                ListItem(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    headlineContent = {
+                        Text(
+                            text = timePassed,
+                            color = MaterialTheme.colorScheme.inverseOnSurface,
+                            fontSize = 12.sp
+                        )
+                    },
+                    leadingContent = {
+                        Text(
+                            text = time,
+                            color = MaterialTheme.colorScheme.inverseOnSurface,
+                            fontSize = 24.sp
+                        )
+                    },
+                    trailingContent = station.nameContentDeparted()
+                )
+                Divider()
+            }
+        }
+
+        is Bus.Boyish -> {
+            Column {
+                ListItem(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    headlineContent = {
+                    },
+                    leadingContent = station.timeContent(time),
+                    trailingContent = station.nameContent()
+                )
+                Divider()
+            }
+        }
+    }
+}
