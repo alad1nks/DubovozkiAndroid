f2d69ea7d2f8554dc9d3c8f44dad7b6c7ef35c84
alad1nks <vasya.sidr777@gmail.com>
2024-03-05 21:09:59 +0300

    feat network: catch 502 error as IOException

--- core/network/src/main/java/com/alad1nks/core/network/retrofit/RetrofitNetwork.kt
+++ core/network/src/main/java/com/alad1nks/core/network/retrofit/RetrofitNetwork.kt
@@ -5,8 +5,11 @@ import com.alad1nks.core.network.model.BusScheduleResponse
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.http.GET
+import java.io.IOException
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -15,13 +18,23 @@ private interface RetrofitNetworkApi {
     suspend fun getBusSchedule(): BusScheduleResponse
 }
 
-private const val BASE_URL = "https://dubovozki.ru/router"
+private const val BASE_URL = "https://dubovozki.ru/router/"
 
 @Singleton
 class RetrofitNetwork @Inject constructor(
     networkJson: Json,
 ) : NetworkDataSource {
+    private val client = OkHttpClient.Builder().addInterceptor { chain ->
+        val requestBuilder = chain.request().newBuilder()
+        val response = chain.proceed(requestBuilder.build())
+        if (response.code == 502) {
+            throw IOException("Server is turned off")
+        }
+        return@addInterceptor response
+    }.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)).build()
+
     private val networkApi = Retrofit.Builder()
+        .client(client)
         .baseUrl(BASE_URL)
         .addConverterFactory(
             networkJson.asConverterFactory("application/json".toMediaType()),
